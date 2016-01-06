1c1caaf4867bf92da5279bc2ba6822547c06f3ad
Priscilla Lok <plok@mba-002206.ad.whirl.net>
2016-01-06 10:45:50 -0800

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Priscilla Lok on 1/6/16.
+//  Copyright (c) 2016 Priscilla Lok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
